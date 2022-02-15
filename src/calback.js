a3f1370fe4da6100eb83509cc79c66ad21b30ccf
fatemeh <you@example.com>
2022-02-15 15:05:08 +0330

    master

---
+++ src/calback.js
@@ -0,0 +1,24 @@
+function ball1(){
+   setTimeout(()=>{
+       console.log('ball one ajrashod');
+   },1000);
+}
+function ball2(){
+    setTimeout(()=>{
+        console.log('ball tow ajrashod');
+    },2000);
+ }
+ function ball3(){
+    setTimeout(()=>{
+        console.log('ball tree ajrashod');
+    },3000);
+ }
+ function ball4(){
+    setTimeout(()=>{
+        console.log('ball for ajrashod');
+    },4000);
+ }
+ball1();
+ball2();
+ball3();
+ball4();
